115e48c1dc164e6ca193e7dd585ebac332c80b24
Heather Lamont <heatherlamontmusic@hotmail.co.uk>
2022-02-24 21:01:52 +0000

    django_meetup template

--- eventbulb/urls.py
+++ eventbulb/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('information.urls')),
+    # path('/django_meetup', include('information.urls')),
     path('admin/', admin.site.urls),
     
 ]
